3507a2091386713a7d93bd27164a05b72b5e3b15
Alex dos Santos <s2it_alsantos@uolinc.com>
2019-11-11 21:08:11 -0300

    Teste Isolado - Rule

--- src/test/java/br/ce/alexleko/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/alexleko/servicos/LocacaoServiceTest.java
@@ -6,7 +6,9 @@ import br.ce.alexleko.entidades.Usuario;
 import br.ce.alexleko.utils.DataUtils;
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
@@ -36,6 +38,30 @@ public class LocacaoServiceTest {
 
 		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+	}
+
+	// =================
+	// = Teste Isolado =
+	// =================
+
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+
+	@Test
+	public void testeLocacao() {
+
+		// = CENÁRIO =
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		// = AÇÃO =
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		// = VERIFICAÇÃO =
+		error.checkThat(locacao.getValor(), is(CoreMatchers.equalTo(6.0)));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
 	}
 
 }
